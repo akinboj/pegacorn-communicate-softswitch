b8e6bb762d091286a4f9b80f0a970a36e1283ad1
akinboj <akinboj@hotmail.com>
2021-09-04 22:46:23 +1000

    Updated for pegacorn whispers

--- start-freeswitch.sh
+++ start-freeswitch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start server.
-sed -i "s/podip/${MY_POD_IP}/g" /etc/freeswitch/sip_profiles/internal.xml
+sed -i "s/internalip/10.0.0.4/g" /etc/freeswitch/sip_profiles/internal.xml
 echo 'Starting Freeswitch...'
 /usr/bin/freeswitch -rp -nf
